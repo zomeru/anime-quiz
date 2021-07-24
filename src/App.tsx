235f82a061f845b4f9b149b5d86239060801578a
Zomer <zomerzxc.19@gmail.com>
2021-07-24 19:31:51 +0800

    Add jsx to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from 'react';
+import QuestionCard from './components/QuestionCard';
 
-interface AppProps {
-
-}
+interface AppProps {}
 
 const App: React.FC<AppProps> = ({}) => {
-    return (<div></div>);
-}
+  const startTrivia = async () => {};
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const nextQuestion = () => {};
+
+  return (
+    <div>
+      <h1>Anime Quiz</h1>
+      <button className='start' onClick={startTrivia}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading Question...</p>
+      <QuestionCard />
+      <button className='next' onClick={nextQuestion}></button>
+    </div>
+  );
+};
 
 export default App;

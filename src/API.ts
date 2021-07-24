90c0295f2ac328d23bb93646341ea3389777a672
Zomer <zomerzxc.19@gmail.com>
2021-07-25 00:16:05 +0800

    Finish workin with API

---
+++ src/API.ts
@@ -0,0 +1,35 @@
+import { API_URL, ANIME_CATEGORY } from './config';
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `${API_URL}amount=${amount}&difficulty=${difficulty}&category=${ANIME_CATEGORY}`;
+  const data = await (await fetch(endpoint)).json();
+
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([
+      ...question.incorrect_answers,
+      question.correct_answer,
+    ]),
+  }));
+};

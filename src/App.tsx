6dcd60856b1d57104d42f23c2c4f357cb9645450
Zomer <zomerzxc.19@gmail.com>
2021-07-24 21:05:19 +0800

    Add logical rendering

--- src/App.tsx
+++ src/App.tsx
@@ -43,25 +43,37 @@ const App: React.FC<AppProps> = ({}) => {
 
   const nextQuestion = () => {};
 
+  const showNextButton =
+    !gameOver &&
+    !loading &&
+    userAnswers.length === questionNumber + 1 &&
+    questionNumber !== TOTAL_QUESTIONS;
+
   return (
     <div>
       <h1>Anime Quiz</h1>
-      <button className='start' onClick={startTrivia}>
-        Start
-      </button>
-      <p className='score'>Score:</p>
-      <p>Loading Question...</p>
-      {/* <QuestionCard
-        questionNumber={questionNumber + 1}
-        totalQuestions={TOTAL_QUESTIONS}
-        question={questions[questionNumber].question}
-        answers={questions[questionNumber].answers}
-        userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
-        callback={checkAnswer}
-      /> */}
-      <button className='next' onClick={nextQuestion}>
-        Next Question
-      </button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+        <button className='start' onClick={startTrivia}>
+          Start
+        </button>
+      ) : null}
+      {!gameOver ? <p className='score'>Score:</p> : null}
+      {loading ? <p>Loading Question...</p> : null}
+      {!loading && !gameOver && (
+        <QuestionCard
+          questionNumber={questionNumber + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[questionNumber].question}
+          answers={questions[questionNumber].answers}
+          userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
+          callback={checkAnswer}
+        />
+      )}
+      {showNextButton ? (
+        <button className='next' onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 };

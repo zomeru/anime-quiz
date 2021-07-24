57947e454da3d01d7da2b813387b8304ff8e21d9
Zomer <zomerzxc.19@gmail.com>
2021-07-24 19:47:08 +0800

    Initialize states and add props to QuestionCard

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QuestionCard from './components/QuestionCard';
 
 interface AppProps {}
 
+const TOTAL_QUESTIONS = 10;
+
 const App: React.FC<AppProps> = ({}) => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [questionNumber, setQuestionNubmber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGamerOver] = useState(true);
+
   const startTrivia = async () => {};
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -18,7 +27,14 @@ const App: React.FC<AppProps> = ({}) => {
       </button>
       <p className='score'>Score:</p>
       <p>Loading Question...</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNumber={questionNumber + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[questionNumber].question}
+        answers={questions[questionNumber].answers}
+        userAnswer={userAnswers ? userAnswers[questionNumber] : undefined}
+        callback={checkAnswer}
+      />
       <button className='next' onClick={nextQuestion}></button>
     </div>
   );
